c92b6bfe33897254f88bbcfc95cec7abb694ea1e
Caleb <1calebtuttle@gmail.com>
2021-06-14 14:22:32 -0400

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='portfoliobuilder',
+    version='0.0.1',
+    install_requires=[
+        'requests',
+        'importlib; python_version == "3.7.6"',
+    ],
+)
