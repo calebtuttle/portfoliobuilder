414c281fb220b0fb858ae9a7fe946ca43b2fc1e6
Caleb <1calebtuttle@gmail.com>
2021-06-20 11:02:35 -0400

    Delete unnecessary code

--- portfoliobuilder/__init__.py
+++ portfoliobuilder/__init__.py
@@ -5,14 +5,8 @@ import os
 
 alpaca_endpoint = 'https://paper-api.alpaca.markets/v2/'
 
-# alpaca_paper_key = input('Enter Alpaca paper API key: ')
-# alpaca_paper_secret = input('Enter Alpaca paper secret: ')
-
-alpaca_paper_key = os.environ['​PORTFOLIOBUILDER_ALPACA_PAPER_KEY']
+alpaca_paper_key = os.environ['PORTFOLIOBUILDER_ALPACA_PAPER_KEY']
 alpaca_paper_secret = os.environ['PORTFOLIOBUILDER_ALPACA_PAPER_SECRET_KEY']
-
-# alpaca_paper_key = 'None'  # Uncomment for testing
-# alpaca_paper_secret = 'None'  # Uncomment for testing
 
 alpaca_headers = {'APCA-API-KEY-ID': alpaca_paper_key, 'APCA-API-SECRET-KEY': alpaca_paper_secret}
 
@@ -21,8 +15,4 @@ alpaca_headers = {'APCA-API-KEY-ID': alpaca_paper_key, 'APCA-API-SECRET-KEY': al
 
 finnhub_endpoint = 'https://finnhub.io/api/v1/'
 
-# finnhub_key = input('Enter Finnhub API key: ')
-
-finnhub_key = os.environ['​PORTFOLIOBUILDER_FINNHUB_KEY']
-
-# finnhub_key = 'None'  # Uncomment for testing
+finnhub_key = os.environ['PORTFOLIOBUILDER_FINNHUB_KEY']
