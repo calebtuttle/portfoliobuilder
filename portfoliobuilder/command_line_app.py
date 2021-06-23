af1c7507316d99d4843ed160c9a68bbed899f72d
Caleb <1calebtuttle@gmail.com>
2021-06-23 18:08:21 -0400

    linkaccount

--- portfoliobuilder/command_line_app.py
+++ portfoliobuilder/command_line_app.py
@@ -1,9 +1,10 @@
 '''
 A command line app for portfoliobuilder.
 '''
-
+import os
 
 from portfoliobuilder import utils, api_utils
+from portfoliobuilder import command_line_utils as cmd_utils 
 from portfoliobuilder.builder import Portfolio, Basket
 from portfoliobuilder.supported_indices import (supported_indices_list, 
                                                 supported_indices_dict)
@@ -15,53 +16,38 @@ print("Welcome to the portfoliobuilder command line application. " +
 help_str = '''Usage: <command>
 
 Commands:
-newportfolio
-listportfolios
-inspectportfolio <portfolio_name>
+account
+linkaccount <alpaca_api_key> <alpaca_secret> 
 newbasket (<symbol0> <symbol1> <symboli>) <weighting_method> <basket_weight>
         weighting_method options: equal market_cap value
 newbasketfromindex <index_symbol> <weighting_method> <basket_weight>
 listbaskets
 inspectbasket <basket_name>
-addbasket <portfolio_name> <basket_name>
 listindices
 
 Enter 'help' to see commands.
 Enter 'q' to quit, or kill with ^c.'''
 
-portfolios = {} # {'portfolio_name': Portfolio object}
 baskets = {} # {'basket_name': Basket object}
 
-def newportfolio():
-    portfolio = Portfolio()
-    portfolio.name = f'Portfolio{len(portfolios)}'
-    portfolios[portfolio.name] = portfolio
-    print(f'{portfolio.name} created.')
+def account():
+    response = api_utils.get_account()
+    for key in response:
+        print(f'{key}: {response[key]}')
 
-def listportfolios():
-    if portfolios:
-        for p_name in portfolios.keys():
-            print(p_name)
-    else:
-        print('No portfolios.')
-
-def inspectportfolio(command):
-    invalid_cmd = command.split(' ')[0] != 'inspectportfolio'
-    invalid_cmd = invalid_cmd or (len(command.split(' ')) > 2)
-    invalid_cmd = invalid_cmd or (len(command.split(' ')) < 2)
-    if invalid_cmd:
-        print('Invalid command.')
+def linkaccount(command):
+    if not cmd_utils.has_num_args(command, 2):
         return
-
-    portfolio_name = command.split(' ')[1]
-    if portfolio_name in portfolios:
-        portfolio = portfolios[portfolio_name]
-        print(f'Inspecting {portfolio_name}...')
-        print(f'Portfolio value: {portfolio.portfolio_value}')
-        print(f'Portfolio cash: {portfolio.cash}')
-        print(f"Portfolio's baskets: {portfolio.baskets}")
-    else:
-        print('Invalid command. Unknown portfolio.')
+    try:
+        alpaca_paper_key = os.environ['PORTFOLIOBUILDER_ALPACA_PAPER_KEY']
+        alpaca_paper_secret = os.environ['PORTFOLIOBUILDER_ALPACA_PAPER_SECRET_KEY']
+    except KeyError:
+        os.environ['PORTFOLIOBUILDER_ALPACA_PAPER_KEY'] = command.split(' ')[1]
+        os.environ['PORTFOLIOBUILDER_ALPACA_PAPER_SECRET_KEY'] = command.split(' ')[2]
+    # Test that the account was actually connected
+    acc = api_utils.get_account()
+    if acc:
+        print(f'Linked to account with id: {acc["id"]}.')
 
 def newbasket(command):
     try:
@@ -99,7 +85,6 @@ def newbasketfromindex(command):
         newbasket(new_command)
     except IndexError:
         print('Invalid command.')
-    
 
 def listbaskets():
     if baskets:
@@ -109,11 +94,10 @@ def listbaskets():
         print('No baskets.')
 
 def inspectbasket(command):
-    invalid_cmd = command.split(' ')[0] != 'inspectbasket'
-    invalid_cmd = invalid_cmd or (len(command.split(' ')) > 2)
-    invalid_cmd = invalid_cmd or (len(command.split(' ')) < 2)
-    if invalid_cmd:
+    if command.split(' ')[0] != 'inspectbasket':
         print('Invalid command.')
+        return
+    if not cmd_utils.has_num_args(command, 1):
         return
 
     basket_name = command.split(' ')[1]
@@ -126,31 +110,6 @@ def inspectbasket(command):
     else:
         print('Invalid command. Unknown basket.')
 
-def addbasket(command):
-    invalid_cmd = command.split(' ')[0] != 'addbasket'
-    invalid_cmd = invalid_cmd or (len(command.split(' ')) > 3)
-    invalid_cmd = invalid_cmd or (len(command.split(' ')) < 3)
-    if invalid_cmd:
-        print('Invalid command.')
-        return
-
-    portfolio_name = command.split(' ')[1]
-    basket_name = command.split(' ')[2]
-    if portfolio_name in portfolios:
-        if basket_name in baskets:
-            portfolio = portfolios[portfolio_name]
-            basket = baskets[basket_name]
-            total_weight = sum([b.weight for b in portfolio.baskets])
-            if basket.weight + total_weight > 100:
-                print(f'Invalid command. {total_weight}% of portfolio is allocated. ' + 
-                        f'Trying to allocate an additional {basket.weight}% of {portfolio_name}.')
-            else:
-                portfolio.baskets.append(basket)
-        else:
-            print('Invalid command. Unknown basket.')
-    else:
-        print('Invalid command. Unknown portfolio.')
-
 def listindices():
     print('Listing indices... (Note: some might not be supported by newbasketfromindex.)')
     print('Index | Symbol')
@@ -160,12 +119,8 @@ def listindices():
 def parse_command(command):
     if command == 'help':
         print(help_str)
-    elif command == 'newportfolio':
-        newportfolio()
-    elif command == 'listportfolios':
-        listportfolios()
-    elif 'inspectportfolio ' in command:
-        inspectportfolio(command)
+    elif command == 'account':
+        account()
     elif 'newbasket ' in command:
         newbasket(command)
     elif 'newbasketfromindex ' in command:
@@ -174,8 +129,6 @@ def parse_command(command):
         listbaskets()
     elif 'inspectbasket ' in command:
         inspectbasket(command)
-    elif 'addbasket' in command:
-        addbasket(command)
     elif command == 'listindices':
         listindices()
     elif command == 'q':
@@ -187,7 +140,7 @@ def parse_command(command):
 # TODO: Implement the ability to read a list of symbols from a file.
 # TODO: Implement buyportfolio, rebalance, savestate (which saves the portfolio to a database or something)
 #       and recoverstate (which reads the saved portfolios into this session's variables).
-# Maybe get rid of portfolio functionality and replace it with a single account (your Alpaca account)?
+# TODO: Implement connection with your Alpaca account
 
 
 command = ''
---
+++ portfoliobuilder/command_line_utils.py
@@ -0,0 +1,11 @@
+
+def has_num_args(command, num_args):
+    '''
+    Note: Pass entire command to this function, not just arguments.
+    Return True if command has exactly num_args arguments, False otherwise.
+    '''
+    split_command = command.split(' ')
+    if len(split_command) == (num_args - 1):
+        return True
+    print('Invalid command. Incorrect number of arguments.')
+    return False
