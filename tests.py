1e7c32eb597b1108b33a5115f221cfeb64d9ac68
Caleb <1calebtuttle@gmail.com>
2021-07-01 13:50:04 -0400

    update tests

--- tests.py
+++ tests.py
@@ -5,36 +5,71 @@ Unit tests.
 from portfoliobuilder import api_utils
 
 
+
+################# api_utils Alpaca tests #################
+
 def test_api_utils_get_account():
     acc = api_utils.get_account()
-    print(acc)
-    
-def test_api_utils_fractionable_tradable():
-    f_and_t_true = api_utils.fractionable_tradable('AAPL')
-    f_and_t_false = api_utils.fractionable_tradable('NOTASYMBOL')
-    assert f_and_t_true
-    assert not f_and_t_false
+    assert acc['currency'] == 'USD'
 
-# api_utils.place_order works
+def test_api_utils_get_asset():
+    asset = api_utils.get_asset('AAPL')
+    assert asset['symbol'] == 'AAPL'
 
-def test_api_utils_get_market_cap():
-    market_cap_aapl = api_utils.get_market_cap('AAPL')
-    market_cap_fake_stock = api_utils.get_market_cap('NOTASYMBOL')
-    
-    assert isinstance(market_cap_aapl, float)
-    assert isinstance(market_cap_fake_stock, None)
+# place_order() works
+
+def test_api_utils_get_position():
+    position = api_utils.get_position('AAPL')
+    assert position['symbol'] == 'AAPL'
+    assert  position['exchange'] == 'NASDAQ'
+
+# close_position() works
+
+
+################# api_utils Finnhub tests #################
+
+
+def test_api_utils_get_profil2():
+    profile = api_utils.get_profile2('AAPL')
+    assert profile['country'] == 'US'
+    assert profile['currency'] == 'USD'
 
 def test_api_utils_get_metrics():
-    metrics_aapl = api_utils.get_metrics('AAPL')
-    metrics_fake_stock = api_utils.get_metrics('NOTASYMBOL')
+    metrics = api_utils.get_metrics('AAPL')
+    assert metrics['symbol'] == 'AAPL'
+    assert isinstance(metrics['metric'], dict)
 
-    assert isinstance(metrics_aapl, dict)
-    assert metrics_fake_stock is None
+def test_api_utils_get_financials_as_reported():
+    fins = api_utils.get_financials_as_reported('AAPL')
+    assert fins['symbol'] == 'AAPL'
+    assert isinstance(fins['data'], list)
 
-def test_api_utils_get_ev_to_fcf():
-    ev_to_fcf_aapl = api_utils.get_ev_to_fcf('AAPL')
-    ev_to_fcf_fake_stock = api_utils.get_ev_to_fcf('NOTASYMBOL')
+def test_api_utils_get_index_constituents():
+    indx_constituents = api_utils.get_index_constituents('^GSPC')
+    assert indx_constituents['symbol'] == '^GSPC'
 
-    assert isinstance(ev_to_fcf_aapl, float)
-    assert ev_to_fcf_fake_stock is None
 
+################# api_utils Polygon tests #################
+
+def test_api_utils_get_financials():
+    fins = api_utils.get_financials('AAPL')
+    assert 'status' in fins.keys()
+    assert 'results' in fins.keys()
+    assert len(fins.keys()) == 2
+
+
+
+
+def run_tests():
+    test_api_utils_get_account()
+    test_api_utils_get_asset()
+    test_api_utils_get_position()
+    test_api_utils_get_profil2()
+    test_api_utils_get_metrics()
+    test_api_utils_get_financials_as_reported()
+    test_api_utils_get_index_constituents()
+    test_api_utils_get_financials()
+
+    print('Tests ran successfully')
+
+run_tests()
